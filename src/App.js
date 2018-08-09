5312402c939905e632a5168e812b29d134fed410
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2018-08-09 11:30:26 +0545

    Add overrides for button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+import Button from './components/button';
+
 class App extends Component {
   render() {
     return (
       <div>
+
+        <Button>Button</Button>
+        <Button primary>Button</Button>
+        <Button secondary>Button</Button>
         <h1>Faire</h1>
       </div>
     );
---
+++ src/components/button/button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Button = ({
+  label,
+  style,
+  children,
+  className = '',
+  primary = false,
+  onClick = f => f,
+  disabled = false,
+  secondary = false,
+}) => <button
+  style={style}
+  onClick={onClick}
+  disabled={disabled}
+  className={`uk-button uk-button-default ${primary ? 'uk-button-primary' : ''} ${secondary ? 'uk-button-secondary' : ''} ${className}`}
+>{label || children}
+  </button>;
+
+export default Button;
