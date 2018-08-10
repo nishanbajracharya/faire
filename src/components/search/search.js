a92c6997dd66a749ab2ef4ec5ae8a8abfcd4149b
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2018-08-10 09:47:16 +0545

    Add search bar styling

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import Icon from '../icon';
+import ICONS from '../../constants/icons';
 
 class Search extends React.Component {
   render() {
-    return <input className="uk-input search-bar" placeholder="Search" />
+    return <div className={`search-container ${this.props.containerClassName}`}>
+      <input className={`uk-input search-bar ${this.props.className}`} placeholder="Search" />
+      <Icon name={ICONS.SEARCH} size={24} className="search-icon" />
+    </div>
   }
+}
+
+Search.defaultProps = {
+  className: '',
+  containerClassName: ''
+}
+
+Search.propTypes = {
+  className: PropTypes.string,
+  containerClassName: PropTypes.string
 }
 
 export default Search;
